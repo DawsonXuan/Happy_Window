f96756545b33d84b4978cd8875f118fbfbbaa929
me1234q <me1234q.me1234q@gmail.com>
2018-12-04 15:47:39 -0800

    Added Button

--- src/Window.java
+++ src/Window.java
@@ -2,11 +2,19 @@
 import java.awt.Toolkit;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
+	
+	private JButton button;
+	
 	public Window() {
 		super("Happy Window");
-		setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		setLayout(new FlowLayout());
+		
+		button = new JButton("Interesting Button");
+		add(button);
 	}
 }
